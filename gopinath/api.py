8f8afce0eab38408948795b3d80f8490c013365e
finbyz <info@finbyz.com>
2020-12-18 06:04:37 +0000

    Changes Updated

--- gopinath/api.py
+++ gopinath/api.py
@@ -4,49 +4,52 @@ from frappe.utils import flt, cint, getdate
 from erpnext.accounts.utils import get_fiscal_year
 import datetime
 
-def pr_before_validate(self,method):
-	pr_cal_rate_qty(self)
+# def pr_before_validate(self,method):
+# 	pr_cal_rate_qty(self)
 	
-def pr_cal_rate_qty(self):
-	for d in self.items:
-		maintain_as_is_stock = frappe.db.get_value("Item",d.item_code,'maintain_as_is_stock')
-		if maintain_as_is_stock:
-			if not d.supplier_concentration:
-				frappe.throw("{} Row: {} Please add supplier concentration".format(d.doctype,d.idx))
-			if not d.concentration:
-				frappe.throw("{} Row: {} Please add concentration".format(d.doctype,d.idx))
+# def pr_cal_rate_qty(self):
+# 	for d in self.items:
+# 		maintain_as_is_stock = frappe.db.get_value("Item",d.item_code,'maintain_as_is_stock')
+# 		if maintain_as_is_stock:
+# 			if not d.supplier_concentration:
+# 				frappe.throw("{} Row: {} Please add supplier concentration".format(d.doctype,d.idx))
+# 			if not d.concentration:
+# 				frappe.throw("{} Row: {} Please add concentration".format(d.doctype,d.idx))
 				
-		if d.get('packing_size') and d.get('no_of_packages') and d.get('tare_weight'):
-			d.qty = d.received_qty = ((d.packing_size - d.tare_weight) * d.no_of_packages)
+# 		if d.get('packing_size') and d.get('no_of_packages'):
+# 			if d.tare_weight:
+# 				d.qty = d.received_qty = ((d.packing_size - d.tare_weight) * d.no_of_packages)
+# 			else:
+# 				d.qty = d.received_qty = (d.packing_size * d.no_of_packages)
 
-			if maintain_as_is_stock:
-				d.quantity = d.qty * d.concentration / 100
-			else:
-				d.quantity = d.qty
+# 			if maintain_as_is_stock:
+# 				d.quantity = d.qty * d.concentration / 100
+# 			else:
+# 				d.quantity = d.qty
 				
-		if d.get('supplier_packing_size') and d.get('supplier_no_of_packages'):
-			d.supplier_qty = (d.supplier_packing_size * d.supplier_no_of_packages)
+# 		if d.get('supplier_packing_size') and d.get('supplier_no_of_packages'):
+# 			d.supplier_qty = (d.supplier_packing_size * d.supplier_no_of_packages)
 
-			if maintain_as_is_stock:
-				d.supplier_quantity = d.supplier_qty * d.supplier_concentration / 100
-			else:
-				d.supplier_quantity = d.supplier_qty
+# 			if maintain_as_is_stock:
+# 				d.supplier_quantity = d.supplier_qty * d.supplier_concentration / 100
+# 			else:
+# 				d.supplier_quantity = d.supplier_qty
 
-		if d.get('accepted_packing_size') and d.get('accepted_no_of_packages'):
-			d.accepted_qty = (d.accepted_packing_size * d.accepted_no_of_packages)
+# 		if d.get('accepted_packing_size') and d.get('accepted_no_of_packages'):
+# 			d.accepted_qty = (d.accepted_packing_size * d.accepted_no_of_packages)
 
-			if maintain_as_is_stock:
-				d.accepted_quantity = d.accepted_qty * d.accepted_concentration / 100
-			else:
-				d.accepted_quantity = d.accepted_qty
+# 			if maintain_as_is_stock:
+# 				d.accepted_quantity = d.accepted_qty * d.accepted_concentration / 100
+# 			else:
+# 				d.accepted_quantity = d.accepted_qty
 
-		if not d.accepted_quantity:
-			d.short_quantity = d.quantity - d .supplier_quantity
-		else:
-			d.short_quantity = d.accepted_quantity - d .supplier_quantity
-		d.supplier_amount = flt(d.supplier_quantity * d.price)
-		d.rate = flt(d.supplier_amount / d.qty)
-		d.amount_difference = flt(d.short_quantity) * flt(d.price)
+# 		if not d.accepted_quantity:
+# 			d.short_quantity = d.quantity - d .supplier_quantity
+# 		else:
+# 			d.short_quantity = d.accepted_quantity - d .supplier_quantity
+# 		d.supplier_amount = flt(d.supplier_quantity * d.price)
+# 		d.rate = flt(d.supplier_amount / d.qty)
+# 		d.amount_difference = flt(d.short_quantity) * flt(d.price)
 def before_naming(self, method):
 	if not self.get('amended_from') and not self.get('name'):
 		date = self.get("transaction_date") or self.get("posting_date") or  self.get("manufacturing_date") or  self.get("date") or getdate()
